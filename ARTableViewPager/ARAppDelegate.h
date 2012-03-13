a87155a3eac294ddc959221c7ffafeca5b27366c
Jonas Stubenrauch <jonas.stubenrauch@arconsis.com>
2012-03-13 19:02:21 +0100

    Initial Commit

---
+++ ARTableViewPager/ARAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ARAppDelegate.h
+//  ARTableViewPager
+//
+//  Created by Jonas Stubenrauch on 13.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ARTableViewPager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ARTableViewPager
+//
+//  Created by Jonas Stubenrauch on 13.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ARAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ARAppDelegate class]));
+    }
+}
